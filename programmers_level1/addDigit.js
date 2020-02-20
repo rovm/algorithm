8aa30f90d2902ef43423b35877c8ae7d7df55659
rovm <rovm92@naver.com>
2020-02-20 14:53:40 +0900

    j
    자릿수 더하기

---
+++ programmers_level1/addDigit.js
@@ -0,0 +1,22 @@
+/* 2020 - 02 - 20
+* 자릿수 더하기
+*/
+
+//방법 1
+function solution(n)
+{
+  var answer = 0;
+  n = n.toString();
+  var nLen = n.length;
+
+  for(let i=0; i<nLen; i++){
+      answer += parseInt(n.charAt(i));
+  }
+  return answer;
+}
+
+//방법 2
+function solution(n)
+{
+  return String(n).split("").reduce((tot,e) => tot + parseInt(e),0);
+}
