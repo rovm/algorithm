fc181999225c717d337cf2c28735486e93c1556f
rovm <rovm92@naver.com>
2020-02-20 15:07:36 +0900

    j
    자연수 뒤집어 배열로 만들기

---
+++ programmers_level1/nReverseToArray.js
@@ -0,0 +1,21 @@
+/* 2020 - 02 - 20
+* 자연수 뒤집어 배열로 만들기
+*/
+
+//방법 1
+function solution(n) {
+    var answer = [];
+    n = n.toString();
+    var len =  n.length-1;
+    
+    for(let i=len; i>=0; i--){
+        answer.push(parseInt(n.charAt(i)));
+    }
+    return answer;
+}
+
+//방법 2
+function solution(n)
+{
+  return String(n).split('').reverse().map(v => +v);
+}
