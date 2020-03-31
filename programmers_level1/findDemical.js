6b92cc0f562da12de0fab40f3e718ddc33266f99
rovm <rovm92@naver.com>
2020-03-31 13:49:29 +0900

    Create findDemical.js

---
+++ programmers_level1/findDemical.js
@@ -0,0 +1,28 @@
+/* 2020 - 02 - 26
+* 소수 찾기
+*/
+
+function solution(n) {
+    let answer = 0;
+    let arr = [];
+
+    for (let i = 2; i <= n; i++) {
+        arr[i] = i;
+    }
+
+    for (let i = 2; i <= n; i++) {
+        if (arr[i] === 0)
+            continue;
+
+        for (let j = i + i; j <= n; j += i) {
+            arr[j] = 0;
+        }
+    }
+
+    for (let i = 2; i <= n; i++) {
+        if (arr[i] !== 0)
+            answer++;
+    }
+
+    return answer
+}
